2c14da4bc2cde2162bf0d3c3a31439b2edb9960a
William Rodríguez <williamrodriguez@Mac-mini-de-William.local>
2023-09-08 21:07:39 +0200

    add nginx to docker

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,11 @@ COPY . .
 
 EXPOSE 3000
 
-# CMD ["npm", "run", "start"]
-CMD ["npm", "run", "dev"]
+CMD ["npm", "run", "start"]
+# CMD ["npm", "run", "dev"]
+
+
+
+
+
+
--- src/config/allowed_origins.js
+++ src/config/allowed_origins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://192.168.1.188:5173',
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'http://149.202.47.11'
 ]
 
 export default allowedOrigins

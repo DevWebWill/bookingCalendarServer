f5467c4d3f811d4b019a438850cb404a8af6b879
William Rodríguez <williamrodriguez@Mac-mini-de-William.local>
2023-09-13 17:55:45 +0200

    allow origins

--- src/config/allowed_origins.js
+++ src/config/allowed_origins.js
@@ -1,9 +1,10 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
-    'http://localhost:5173',
-    'http://192.168.1.188:5173',
     'http://localhost:8080',
-    'http://149.202.47.11'
+    'http://booxita.com',
+    'https://booxita.com',
+    'http://localhost:8081',
+    'https://localhost:8081',
 ]
 
 export default allowedOrigins
